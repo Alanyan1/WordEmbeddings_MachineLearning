559fed5f0964ec0215e83ba6a84afbd12f9d033c
samEmi <oemilolo@gitlab.cs.ucl.ac.uk>
2020-08-04 13:28:26 +0100

    Developing Processor

--- Preprocessor.py
+++ Preprocessor.py
@@ -1,8 +1,25 @@
 
 
 class Processor:
-    def __init__(self):
-        return
-        pass
+    def __init__(self, datasetPath, numberOfComments):
+        self.datasetPath = datasetPath
+        self.numberOfComments = numberOfComments
+        self.currentCommentList = None
+
+    def __iter__(self):
+        """
+        Yields result of data-preprocessing to a model.
+        """
+        with open(self.datasetPath, "r") as file:
+            for recipeLine in range(self.numberOfComments):
+                currentComment = file.readline()
+                currentCommentList = currentComment.replace("\n", "").split(",")
+                yield currentCommentList
 
 
+
+#Example Use
+# process = Processor("dataset.txt", 2)
+#
+# for element in process:
+#     print(element)
